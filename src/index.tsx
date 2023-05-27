3dfa1c551c9c527dc9e82e73d5e98fa0aab2dd2b
Louis Aussedat <aussedat.louis@gmail.com>
2023-05-27 19:11:24 +0200

    src/App: use functions and hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RuleControl } from './App';
+import RuleControl from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <RuleControl />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
